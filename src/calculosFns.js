d8c1d854ff6966c139042a248ed1b1d9bb8a8145
Tomas Ruiz <tomialeruiz@gmail.com>
2025-05-13 17:21:37 -0300

    Empece a hacer las funciones para proporcionar al index.

---
+++ src/calculosFns.js
@@ -0,0 +1,11 @@
+const funciones=[
+    {
+        name:"suma",
+        value:(numeros)=> {
+            let total = 0;
+            for (let n of numeros) total += n;
+            return total;
+        },
+    },
+    {}
+]
